990f9386fe7ad06745c91e6bfbcd9b40f567a837
Mirian Santana <miriansantana93@gmail.com>
2023-03-14 22:34:32 -0300

    Added DataSource on class ViewController

--- ProjectRequestPizza/ProjectRequestPizza/ViewController.swift
+++ ProjectRequestPizza/ProjectRequestPizza/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? PizzaTableViewCell {
+            
+            
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+  
+}
